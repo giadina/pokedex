b36ee79c6552e38c1be67adbec89c2c57452fe2b
Giada Tacconelli <giadatacconelli@MBP-di-Giada.home>
2021-09-13 23:11:13 +0200

    Forcing Jackson to return isLegendary key during deserialization. Due to how it work, it was removing the is prefix

--- pokedex-api/src/main/java/com/pokedex/models/responses/PokemonInfoResponse.java
+++ pokedex-api/src/main/java/com/pokedex/models/responses/PokemonInfoResponse.java
@@ -1,5 +1,6 @@
 package com.pokedex.models.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class PokemonInfoResponse {
     private String name;
+    @JsonProperty("isLegendary")
     private boolean isLegendary;
     private String habitat;
     private String description;
